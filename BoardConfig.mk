99eaf460f5ca86b8238b165297b44afbe53eba65
williamsado <williamsado1004@gmail.com>
2024-01-18 03:54:31 +0800

    mars: Add MiuiCamera support
    mars: Patch libwa_sat to load libion from vendor
    This seems to be used by stock camera.
    
    01-31 09:29:05.661   895 17066 E linker  : library "/system/lib64/libion.so" ("/system/lib64/libion.so") needed or dlopened by "/vendor/lib64/libwa_sat.so" is not accessible for the namespace: [name="vndk", ld_library_paths="", default_library_paths="/apex/com.android.vndk.v33/lib64:/vendor/lib64", permitted_paths="/odm/lib64/hw:/odm/lib64/egl:/vendor/lib64/hw:/vendor/lib64/egl:/system/vendor/lib64/hw:/system/vendor/lib64/egl:/apex/com.android.vndk.v33/lib64/hw"]
    01-31 09:29:05.661   895 17066 E walog/IonImageAlloc: Error dlopen in Library: /system/lib64/libion.so
    01-31 09:29:05.661   895 17066 E walog/IonImageAlloc: Failed to open ion device
    
    Change-Id: Ic77e7342aaf4c2c2b7e27e35767b59990dd95b44
    
    Signed-off-by: RobertGarciaa <chae0218@naver.com>
    
    tmp

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,6 +6,9 @@
 
 DEVICE_PATH := device/xiaomi/mars
 
+# Inherit from proprietary files for Meme Camera
+-include vendor/xiaomi/mars-miuicamera/products/board.mk
+
 BUILD_BROKEN_DUP_RULES := true
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
--- device.mk
+++ device.mk
@@ -17,14 +17,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_ven
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
-# Inherit Google Camera
-$(call inherit-product-if-exists, vendor/MGC/config.mk)
-
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/mars/mars-vendor.mk)
 
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
+# Call the Meme Camera setup
+$(call inherit-product-if-exists, vendor/xiaomi/mars-miuicamera/products/miuicamera.mk)
 
 # API
 PRODUCT_SHIPPING_API_LEVEL := 30
