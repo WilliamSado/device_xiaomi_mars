bc70cca2ff5f111d25369c064833c225eef37d52
WilliamSado <williamsado1004@gmail.com>
2024-04-23 22:20:25 +0800

    mars: Build common share

--- device.mk
+++ device.mk
@@ -21,6 +21,10 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+# PowerShare
+PRODUCT_PACKAGES += \
+    vendor.aospa.powershare-service
+
 # Sensors
 PRODUCT_PACKAGES += \
     sensors.xiaomi.mars
