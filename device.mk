cdad70ff68b0d6f621a8f3205ac6f1a96884a601
WilliamSado <williamsado1004@gmail.com>
2024-04-23 22:41:27 +0800

    mars: overlay: Reboard to aospa

--- device.mk
+++ device.mk
@@ -19,7 +19,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-aospa
 
 # PowerShare
 PRODUCT_PACKAGES += \
